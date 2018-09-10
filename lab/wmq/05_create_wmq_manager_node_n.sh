4d8734ad4bc9a249bbffe69a8a880cf36fd2c4c8
Bmoreira90 <43045383+Bmoreira90@users.noreply.github.com>
2018-09-10 18:52:20 -0300

    Update 05_create_wmq_manager_node_n.sh

--- lab/wmq/05_create_wmq_manager_node_n.sh
+++ lab/wmq/05_create_wmq_manager_node_n.sh
@@ -1,14 +1,25 @@
-#Creating QM in the cluster
+#!/bin/sh
+
 QMGR=$(hostname | tr -dc '[:alnum:]\n\r' | tail -c 8 | tr '[:lower:]' '[:upper:]')
+
 #Creating QM in the cluster
 
 echo "crtmqm -u SYSTEM.$QMGR.DEAD.LETTER.QUEUE $QMGR"
 echo "strmqm $QMGR"
 
-echo DEF CHANNEL(TO.%QMGR%.TMX01) CHLTYPE(CLUSRCVR) TRPTYPE(TCP) CONNAME('CONNAME()') CLUSTER(TMX01) >%QMGR%.in
-echo DEF CHANNEL(TO.TMXREPO01.TMX01) CHLTYPE(CLUSSDR) TRPTYPE(TCP) CONNAME('CONNAME(16000)') CLUSTER(TMX01) >%QMGR%.in
+echo DEF CHANNEL(TO.%QMGR%.TMX01) \
+		CHLTYPE(CLUSRCVR) TRPTYPE(TCP) \
+		CONNAME('CONNAME()') \
+		CLUSTER(TMX01) >%QMGR%.in
+		
+echo DEF CHANNEL(TO.TMXREPO01.TMX01) \
+		CHLTYPE(CLUSSDR) \
+		TRPTYPE(TCP) \
+		CONNAME('CONNAME(16000)') \
+		CLUSTER(TMX01) >%QMGR%.in
 
 runmqsc %QMGR% <%QMGR%.in >%QMGR%.out
+
 type %QMGR%.out
 
 pause
