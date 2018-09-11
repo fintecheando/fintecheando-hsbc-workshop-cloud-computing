636833aadb3a570eb34452169db8fc2658afb550
Bmoreira90 <43045383+Bmoreira90@users.noreply.github.com>
2018-09-11 19:48:59 -0300

    Update 05_create_wmq_manager_node_n.sh

--- lab/wmq/05_create_wmq_manager_node_n.sh
+++ lab/wmq/05_create_wmq_manager_node_n.sh
@@ -4,23 +4,23 @@ QMGR=$(hostname | tr -dc '[:alnum:]\n\r' | tail -c 8 | tr '[:lower:]' '[:upper:]
 
 #Creating QM in the cluster
 
-echo "crtmqm -u SYSTEM.$QMGR.DEAD.LETTER.QUEUE $QMGR"
-echo "strmqm $QMGR"
+crtmqm -u SYSTEM.$QMGR.DEAD.LETTER.QUEUE $QMGR
+strmqm $QMGR
 
-echo DEF CHANNEL(TO.%QMGR%.TMX01) \
-		CHLTYPE(CLUSRCVR) TRPTYPE(TCP) \
-		CONNAME('fintecheando-wmq-repo-01.bitnamiapp.com(16000)') \
-		CLUSTER(TMX01) >%QMGR%.in
+echo "DEF CHANNEL(TO.%QMGR%.TMX01)" \
+		"CHLTYPE(CLUSRCVR) TRPTYPE(TCP)" \
+		"CONNAME('fintecheando-wmq-repo-01.bitnamiapp.com(16000)')" \
+		"CLUSTER(TMX01)" >%QMGR%.in
 		
-echo DEF CHANNEL(TO.TMXREPO01.TMX01) \
-		CHLTYPE(CLUSSDR) \
-		TRPTYPE(TCP) \
-		CONNAME('fintecheando-wmq-repo-01.bitnamiapp.com(16000)') \
-		CLUSTER(TMX01) >%QMGR%.in
+echo "DEF CHANNEL(TO.TMXREPO01.TMX01)" \
+		"CHLTYPE(CLUSSDR)" \
+		"TRPTYPE(TCP)" \
+		"CONNAME('fintecheando-wmq-repo-01.bitnamiapp.com(16000)')" \
+		"CLUSTER(TMX01)" >%QMGR%.in
 		
-echo DEF LISTENER(LSTR.16000) TRTYPE(TCP) PORT (16000) CONTROL(QMGR) >%QMGR%.in
+echo "DEF LISTENER(LSTR.16000) TRTYPE(TCP) PORT (16000) CONTROL(QMGR)" >%QMGR%.in
 
-runmqsc %QMGR% <%QMGR%.in >%QMGR%.out
+runmqsc $QMGR <%QMGR%.in >%QMGR%.out
 
 type %QMGR%.out
 
